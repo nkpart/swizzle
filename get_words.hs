d929f2f53c9c7368bfd2dc62e406d455614d40b0
Nick Partridge <nkpart@gmail.com>
2011-06-11 11:58:21 +1000

    sortedword is good

--- get_words.hs
+++ get_words.hs
@@ -11,29 +11,27 @@ import System.IO
 import Data.Char (isLower)
 import Data.List ((\\), sort)
 
--- Dont allow duplicate letters
-isSubset :: String -> String -> Bool
--- isSubset little big = null $ little \\ big
-isSubset little big = isSubset' little big
+newtype SortedWord = SortedWord (String, String) deriving Eq
+instance Show SortedWord where show (SortedWord (a,_)) = show a
+sortedWord s = SortedWord (s, sort s)
+
+isSubset :: SortedWord -> String -> Bool
+isSubset (SortedWord (_, little)) big = isSubset' little big
 
 isSubset' :: String -> String -> Bool
 isSubset' [] _ = True
 isSubset' (_:_) [] = False
-isSubset' (x:xs) (y:ys) = let
-  equal = x == y
-  checkLater = x > y
-  restYs = if checkLater then y:ys else ys
-  in (equal && isSubset' xs ys) || (checkLater && isSubset' (x:xs) ys)
+isSubset' (x:xs) (y:ys) = (x == y && isSubset' xs ys) || (x > y && isSubset' (x:xs) ys)
 
 goodWord [] = False
 goodWord (x:[]) = False
 goodWord (x:y:[]) = False
 goodWord xs = isLower $ head xs -- this is the best check for improving performance
 
-type WordIndex = IM.IntMap (M.Map Char [String])
+type WordIndex = IM.IntMap (M.Map Char [SortedWord])
 
-addWord' :: String -> M.Map Char [String] -> M.Map Char [String]
-addWord' word m = M.alter (Just . z (sort word)) (head word) m
+addWord' :: String -> M.Map Char [SortedWord] -> M.Map Char [SortedWord]
+addWord' word m = M.alter (Just . z (sortedWord word)) (head word) m
   where z f = maybe [f] (f:)
 
 addWord :: String -> WordIndex -> WordIndex
@@ -43,13 +41,13 @@ addWord word index = IM.alter (Just . z word) (length word) index
 sizeIndex :: [String] -> WordIndex
 sizeIndex words = foldl' (\m word -> addWord word m) IM.empty $ filter goodWord words
 
-findSize :: Int -> WordIndex -> M.Map Char [String]
+findSize :: Int -> WordIndex -> M.Map Char [SortedWord]
 findSize n = fromMaybe M.empty . IM.lookup n 
 
-grabJust :: String -> Int -> WordIndex -> [String]
+grabJust :: String -> Int -> WordIndex -> [SortedWord]
 grabJust xs n foo = (concat $ map (\k -> M.findWithDefault [] k $ findSize n foo) xs)
 
-grabAll :: Int -> WordIndex -> [String]
+grabAll :: Int -> WordIndex -> [SortedWord]
 grabAll n foo = concat . M.elems $ findSize n foo 
 
 main = do
@@ -58,7 +56,7 @@ main = do
   let index = sizeIndex words
   let sixers = filter (\w -> goodWord w && length w == 6)  words
   withFile "data" WriteMode $ \h -> do
-    forM_ (take 10 sixers) $ \word -> do
+    forM_ (sixers) $ \word -> do
       let rest = map (\n -> grabJust word n index) [3..5]
       hPrint h (word, map (filter (`isSubset` (sort word))) rest)
 
